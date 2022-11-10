3c58bb8bae0225bff8d07feb6c5deea25b43a5f7
Emma Wiggans <ea.wiggans@gmail.com>
2022-11-10 07:49:01 -0500

    beginnings of big function with conditional setup

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var charLenInput = prompt("Choose your character length (between 8 and 128)");
+  var lengInp = Number(charLenInput);
+  console.log(lengInp);
+  if (lengInp < 8 || lengInp > 129) {
+    alert("Please choose a number from 8-128");
+    return;
+  }
+  else {
+
+  }
+  var lowInp = confirm("Would you like to use lowercase letters?");
+    if (lowInp) {
+      var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    }
+    else {
+
+    }
+  var lowInp = confirm("Would you like to use uppercase letters?");
+    if (lowInp) {
+      var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    }
+    else {
+
+    }
+  var numInp = confirm("Would you like to use numbers?");
+  if (numInp) {
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  }
+  else {
+
+  }
+  var charInp = confirm("Would you like to use special characters?");
+  if (charInp) {
+    var speChar = ["!", "@", "#", "$", "%", "&"];
+  }
+  else {
+
+  }
+
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -21,11 +65,26 @@ generateBtn.addEventListener("click", writePassword);
 // THEN I select which criteria to include in the password
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
+
+
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
+// confirm("Do you want to inclue lowercase letters?");
+// // if (confirm) {
+// //   USE LOWERCASE VAR
+// // }
+
+// confirm("Do you want to include UPPERCASE LETTERS?")
+// // if ()
+// confirm(numeric values)
+// confirm(special characters)
+// // WHEN I answer each prompt
+// // THEN my input should be validated and at least one character type should be selected
+// 
+// var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// var speChar = ["!", "@", "#", "$", "%", "&"];
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+
+// math.random is applied to all selected variable arrays to pull the selected number of characters randomly
